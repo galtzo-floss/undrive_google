896f45c6b9dd40729770b61a1eb5828b30132ff0
Peter H. Boling <peter.boling@gmail.com>
2025-10-20 10:29:22 -0600

    ✅ skip jruby for output capture spec

--- spec/support/shared_examples/cli_output.rb
+++ spec/support/shared_examples/cli_output.rb
@@ -2,6 +2,7 @@
 
 RSpec.shared_examples_for "cli output" do |expected|
   it "is printed" do
+    skip_for(engine: :jruby, reason: "output capturing not supported on JRuby")
     block_is_expected.to not_output.to_stderr_from_any_process &
       output(a_string_including(expected)).to_stdout_from_any_process
   end
