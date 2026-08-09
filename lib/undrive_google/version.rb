8485df98d36cb94bb370dea8273ad33b61eda353
Peter H. Boling <peter.boling@gmail.com>
2026-08-08 21:07:21 -0600

    🔖 Bump gem version

--- lib/undrive_google/version.rb
+++ lib/undrive_google/version.rb
@@ -4,7 +4,7 @@ module UndriveGoogle
   # Version namespace for this gem.
   module Version
     # Current gem version.
-    VERSION = "1.1.2"
+    VERSION = "1.1.3"
   end
   # Current gem version exposed at the traditional constant location.
   VERSION = Version::VERSION # Traditional Constant Location
